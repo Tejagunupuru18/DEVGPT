282702924c51406d7b232be7bc0bd183376b5520
TEJA <tejagunupuru200@gmail.com>
2026-06-06 09:47:27 +0530

    server is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import Sidebar from "./components/Sidebar.jsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Chatbox from "./components/Chatbox.jsx";
 import Credits from "./pages/Credits.jsx";
 import Community from "./pages/Community.jsx";
+import { assets } from "./assets/assets.js";
+import "./assets/prism.css";
+import { useState } from "react";
+import Loading from "./pages/Loading.jsx";
+import { useAppContext } from "./context/AppContext.jsx";
+import Login from "./pages/Login.jsx";
 const App = () => {
+  const { user } = useAppContext();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { pathname } = useLocation();
+  if (pathname === "/loading") {
+    return <Loading />;
+  }
   return (
     <>
-      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
-        <div className="flex h-screen w-screen">
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<Chatbox />} />
-            <Route path="/credits" element={<Credits />} />
-            <Route path="/community" element={<Community />} />
-          </Routes>
+      {!isMenuOpen && (
+        <img
+          src={assets.menu_icon}
+          className="w-6 h-6 absolute top-4 left-4 cursor-pointer not-dark:invert md:hidden"
+          onClick={() => setIsMenuOpen(true)}
+          alt=""
+        />
+      )}
+      {user ? (
+        <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+          <div className="flex h-screen w-screen">
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Routes>
+              <Route path="/" element={<Chatbox />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div classname="bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen">
+          <Login />
+        </div>
+      )}
     </>
   );
 };
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,148 @@
 import React from "react";
+import { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets.js";
+import moment from "moment";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
+  const { chats, setSelectedChat, theme, setTheme, user, navigate } =
+    useAppContext();
+  console.log("Chats:", chats);
+  const [search, setSearch] = useState("");
+  return (
+    <div
+      className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r-[#806095]/30 backdrop-blur-3xl 
+  transition-all duration-500 max-md:absolute left-0 z-1 ${!isMenuOpen && `max-md:-translate-x-full`}`}
+    >
+      <img
+        src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+        alt=""
+        className="w-full max-w-48"
+      />
+      {/* New Chat Button */}
+      <button
+        className="flex justify-center items-center w-full py-2 mt-10
+        text-white bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer"
+      >
+        <span className="mr-2 text-xl">+</span>New Chat
+      </button>
+      {/* Search Conversation*/}
+      <div className="flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md">
+        <img src={assets.search_icon} className="w-4 not-dark:invert" alt="" />
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          type="text"
+          placeholder="Search Conversations"
+          className="text-xs placeholder:text-gray-400 outline-none"
+        />
+      </div>
+      {/* Recent Chats */}
+      {chats.length > 0 && <p className="mt-4 text-sm"> Recent Chats</p>}
+      <div className="flex-1 min-h-0 overflow-y-auto mt-3 text-sm space-y-3">
+        {chats
+          .filter((chat) =>
+            chat.messages[0]
+              ? chat.messages[0]?.content
+                  .toLowerCase()
+                  .includes(search.toLowerCase())
+              : chat.name.toLowerCase().includes(search.toLowerCase()),
+          )
+          .map((chat) => (
+            <div
+              onClick={() => {
+                navigate("/");
+                setSelectedChat(chat);
+                setIsMenuOpen(false);
+              }}
+              key={chat._id}
+              className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
+            >
+              <div>
+                <p className="truncate w-full">
+                  {chat.messages.length > 0
+                    ? chat.messages[0].content.slice(0, 32)
+                    : chat.name}
+                </p>
+                <p className="text-xs text-gray-500 dark:text-[#B1A6C0]">
+                  {moment(chat.updatedAt).fromNow()}
+                </p>
+              </div>
+              <img
+                src={assets.bin_icon}
+                className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"
+                alt=""
+              />
+            </div>
+          ))}
+      </div>
+      {/* community images*/}
+      <div
+        onClick={() => {
+          navigate("/community");
+          setIsMenuOpen(false);
+        }}
+        className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md curser-pointer hover:scale-103 transition-all"
+      >
+        <img src={assets.gallery_icon} className="w-4 not-dark:invert" alt="" />
+        <div className="flex flex-col text-sm">
+          <p>Community Images</p>
+        </div>
+      </div>
+      {/* credits*/}
+      <div
+        onClick={() => {
+          navigate("/credits");
+          setIsMenuOpen(false);
+        }}
+        className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md curser-pointer hover:scale-103 transition-all"
+      >
+        <img src={assets.diamond_icon} className="w-4 dark:invert" alt="" />
+        <div className="flex flex-col text-sm">
+          <p>Credits:{user?.credits}</p>
+          <p className="text-sm text-gray-400">
+            Purchase credits to use QuickGpt
+          </p>
+        </div>
+      </div>
+      {/* Dark Mode toggle*/}
+      <div className="flex items-center justify-between gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md ">
+        <div className="flex items-center gap-2 text-sm">
+          <img src={assets.theme_icon} className="w-4 not-dark:invert" alt="" />
+          <p>Dark Mode</p>
+        </div>
+        <label className="relative inline-flex  cursor-pointer">
+          <input
+            onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+            type="checkbox"
+            className="sr-only peer"
+            checked={theme === "dark"}
+          />
+          <div className="w-9 h-5 bg-gray-400 peer-checked:bg-purple-600 transition-all"></div>
+          <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
+        </label>
+      </div>
+      {/* User account*/}
+      <div className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md curser-pointer group">
+        <img src={assets.user_icon} className="w-7 rounded-full" alt="" />
+        <p className="flex-1 text-sm dark:text-primary truncate">
+          {user ? user.name : "Login your account"}
+        </p>
+        {user && (
+          <img
+            className="h-5 curser-pointer hidden not-dark:invert group-hover:block"
+            src={assets.logout_icon}
+          />
+        )}
+      </div>
+      <img
+        onClick={() => setIsMenuOpen(false)}
+        src={assets.close_icon}
+        className="absolute top-3 right-3 w-5 h-5 curser-pointer md:hidden not-dark:invert"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Sidebar;
